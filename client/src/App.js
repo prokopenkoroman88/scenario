e1ec00391c1cd695a99825fe517e4b1456db7d00
Роман Прокопенко <prokopenkoroman88@gmail.com>
2022-12-05 19:57:54 +0200

    Ініціалізація Клієнта

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useRef } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Client from './features/Client.js'
+
+function App() {
+	return (
+		<div className="App">
+			<Client
+			>
+			</Client>
+		</div>
+	);
+}
+
+export default App;
